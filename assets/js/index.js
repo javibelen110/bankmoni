b54de7f007860eaab336830e95a89251da0a0afb
joarayaa <jviverosa@estudiante.uc.cl>
2026-05-12 00:03:18 -0400

    modo oscuro

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,20 @@ const btnSalir = document.getElementById("btnsalir")
 const section1 = document.getElementById("sectionLogin")
 const section2 = document.getElementById("sectionOperacion")
 
+const temaOscuro = () => {
+    document.querySelector("html").setAttribute("data-bs-theme","dark");
+    document.querySelector("#dl-icon").setAttribute("class","bi bi-sun-fill");
+}
+
+const temaClaro = () => {
+    document.querySelector("html").setAttribute("data-bs-theme","light");
+    document.querySelector("#dl-icon").setAttribute("class","bi bi-moon-fill");
+}
+
+const cambiarTema = () => {
+    document.querySelector("html").getAttribute("data-bs-theme") === "light"?
+    temaOscuro() : temaClaro()
+}
 
 const password = "1234"
 const nomCliente = "luis"
